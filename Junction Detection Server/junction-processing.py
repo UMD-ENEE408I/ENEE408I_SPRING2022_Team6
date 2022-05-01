506ee4f33d27e38cf4ed3e6ced2b65b7d2248b4f
Derien Scott <elids3@umd.edu>
2022-04-30 22:03:50 -0400

    Update junction-processing.py

--- Junction Detection Server/junction-processing.py
+++ Junction Detection Server/junction-processing.py
@@ -91,63 +91,63 @@ def get_junction(mouse):
 
 
 def get_vip(mouse):
-            # Get a reference to webcam #0 (the default one)
-            video_capture = cv2.VideoCapture(0)
-            
-            name = ''
-         
-            while True:
-                # Grab a single frame of video
-                ret, frame = video_capture.read()
+    # Get a reference to webcam #0 (the default one)
+    video_capture = cv2.VideoCapture(0)
+    
+    name = ''
+    
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
 
-                #small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
+        #small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
 
-                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-                frame2 = frame[:, :, ::-1]
-                frame3 = cv2.resize(frame2, (900,900), fx=5, fy=5)
-                # frame3 = cv2.cvtColor(frame4, cv2.COLOR_BGR2GRAY)
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        frame2 = frame[:, :, ::-1]
+        frame3 = cv2.resize(frame2, (900,900), fx=5, fy=5)
+        # frame3 = cv2.cvtColor(frame4, cv2.COLOR_BGR2GRAY)
 
-                # Find all the faces and face enqcodings in the frame of video
-                face_locations = face_recognition.face_locations(frame3)
-                face_encodings = face_recognition.face_encodings(frame3, face_locations)
+        # Find all the faces and face enqcodings in the frame of video
+        face_locations = face_recognition.face_locations(frame3)
+        face_encodings = face_recognition.face_encodings(frame3, face_locations)
 
-                # Loop through each face in this frame of video
-                for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-                    # See if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        # Loop through each face in this frame of video
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
 
-                    name = "Unknown"
+            name = "Unknown"
 
-                    # If a match was found in known_face_encodings, just use the first one.
-                    # if True in matches:
-                    #     first_match_index = matches.index(True)
-                    #     name = known_face_names[first_match_index]
+            # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
 
-                    # Or instead, use the known face with the smallest distance to the new face
-                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                    best_match_index = np.argmin(face_distances)
-                    if matches[best_match_index]:
-                        name = known_face_names[best_match_index]
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
 
-                    # Draw a box around the face
-                    cv2.rectangle(frame3, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a box around the face
+            cv2.rectangle(frame3, (left, top), (right, bottom), (0, 0, 255), 2)
 
-                    # Draw a label with a name below the face
-                    cv2.rectangle(frame3, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                    font = cv2.FONT_HERSHEY_DUPLEX
-                    cv2.putText(frame3, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame3, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame3, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-                # Display the resulting image
-                cv2.imshow('Video', frame3)
+        # Display the resulting image
+        cv2.imshow('Video', frame3)
 
 
-                # Hit 'q' on the keyboard to quit!
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-            # Release handle to the webcam
-            video_capture.release()
-            cv2.destroyAllWindows()
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
 
     # return name
     return name
